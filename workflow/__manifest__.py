9726c9707b3b88ffcac61f85d8d58b420bc3cf0a
DucDung <dung.nd207946@gmail.com>
2024-12-17 10:08:03 +0700

    Add view state in model

--- workflow/__manifest__.py
+++ workflow/__manifest__.py
@@ -5,12 +5,13 @@
     'data': [
         'security/ir.model.access.csv',
         'views/custom_workflow_views.xml',
+        'views/state_in_model_views.xml',
     ],
     'assets': {
         'web.assets_backend': [
              # Phải thêm đường dẫn của phần JS được import từ module gốc
-            # 'workflow/static/src/**/*',
-            ('after', 'web/static/src/views/**/*', 'workflow/static/src/**/*')
+            'workflow/static/src/**/*',
+            # ('after', 'web/static/src/views/**/*', 'workflow/static/src/**/*')
         ],
     },
     'application': True,
--- workflow/models/state_in_model.py
+++ workflow/models/state_in_model.py
@@ -3,6 +3,6 @@ from odoo import models, fields, api
 class StateInModel(models.Model):
     _name = 'state.in.model'
     _description = "State in certain Model"
-    name = fields.Char(string='Name')
+    name = fields.Char(string='State')
     model_id = fields.Many2one('ir.model', string='Model', ondelete='cascade', store=True, required=True)
 
